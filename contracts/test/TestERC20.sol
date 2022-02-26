f6e1f38e84d56e4416bc5443f83845eae4fcd053
0xandrew <andrew@addresszero.org>
2022-02-26 11:19:54 +0530

    ft: create test ERC20 token

---
+++ contracts/test/TestERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor(uint256 amountToMint) ERC20("Test ERC20", "TEST") {
+        _mint(msg.sender, amountToMint);
+    }
+}
